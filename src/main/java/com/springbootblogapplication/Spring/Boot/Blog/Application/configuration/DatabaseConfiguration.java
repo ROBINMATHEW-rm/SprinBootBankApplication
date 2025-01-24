7e15104f47547dd0f72aad1b49e239cbb9638ef8
ROBINMATHEW-rm <robinmathew619619@gmail.com>
2025-01-25 00:01:00 +0530

    Code Changes

--- src/main/java/com/springbootblogapplication/Spring/Boot/Blog/Application/configuration/DatabaseConfiguration.java
+++ src/main/java/com/springbootblogapplication/Spring/Boot/Blog/Application/configuration/DatabaseConfiguration.java
@@ -1,8 +1,28 @@
 package com.springbootblogapplication.Spring.Boot.Blog.Application.configuration;
 
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
 
+@Configuration
 public class DatabaseConfiguration {
+@Value("${spring.datasource.url}")
+ private  String url;
+@Value("${spring.datasource.username}")
+private  String userName;
+@Value("${spring.datasource.password}")
+private  String password;
+
+@Bean
+public DataSource dataSource(){
+    HikariDataSource dataSource = new HikariDataSource();
+    dataSource.setJdbcUrl(url);
+    dataSource.setUsername(userName);
+    dataSource.setPassword(password);
+    return  dataSource;
+}
 
 }
