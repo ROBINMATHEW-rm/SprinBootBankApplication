18cedc65f31fb1178cf4ccd06442e6441c1d8424
ROBINMATHEW-rm <robinmathew619619@gmail.com>
2025-02-22 23:06:30 +0530

    New changes

--- src/main/java/com/springbootblogapplication/Spring/Boot/Blog/Application/controller/AddressController.java
+++ src/main/java/com/springbootblogapplication/Spring/Boot/Blog/Application/controller/AddressController.java
@@ -4,17 +4,17 @@ import com.springbootblogapplication.Spring.Boot.Blog.Application.dto.AddressDto
 import com.springbootblogapplication.Spring.Boot.Blog.Application.service.AddressService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+@RequestMapping("v1")
 @RestController
 public class AddressController {
     @Autowired
     private AddressService addressService;
-    @PostMapping("address/add")
+    @PostMapping("/address")
     public ResponseEntity<?> addressAdd(@RequestBody AddressDto addressDto){
         AddressDto result=addressService.addressAdd(addressDto);
         return ResponseEntity.status(HttpStatus.CREATED).body(result);
