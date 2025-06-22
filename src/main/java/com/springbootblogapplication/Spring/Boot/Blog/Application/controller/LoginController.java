4bc15e626ff91dfc34705aee90c7bbc4173bca51
Robin Mathew <robinmathew619619@gmail.com>
2025-06-22 15:57:54 +0530

    Updated Files

---
+++ src/main/java/com/springbootblogapplication/Spring/Boot/Blog/Application/controller/LoginController.java
@@ -0,0 +1,9 @@
+package com.springbootblogapplication.Spring.Boot.Blog.Application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("v1")
+@RestController
+public class LoginController {
+}
---
+++ src/main/java/com/springbootblogapplication/Spring/Boot/Blog/Application/jwt/JwtFilter.java
@@ -0,0 +1,45 @@
+package com.springbootblogapplication.Spring.Boot.Blog.Application.jwt;
+
+import com.springbootblogapplication.Spring.Boot.Blog.Application.service.impl.LoginServiceImpl;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JwtFilter extends OncePerRequestFilter {
+    @Autowired
+    private JwtUtils jwtUtils;
+    @Autowired
+    private LoginServiceImpl loginService;
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+         String authHeader = request.getHeader("Authorization");
+         String token =null;
+         String email =null;
+
+         if(authHeader !=null && authHeader.startsWith("Bearer")){
+             token =authHeader.substring(7);
+             email =jwtUtils.extractEmail(token);
+         }
+
+         if(email != null && SecurityContextHolder.getContext().getAuthentication() == null){
+               User useDetails = (User) loginService.loadUserByUsername(email);
+               if(jwtUtils.validateToken(token)){
+                   UsernamePasswordAuthenticationToken authenticationToken =
+                           new UsernamePasswordAuthenticationToken(useDetails,null,useDetails.getAuthorities());
+                   SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+               }
+         }
+         filterChain.doFilter(request,response);
+    }
+}
